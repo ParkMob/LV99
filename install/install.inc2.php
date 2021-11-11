553695060061cc80ebff129f01e5281ea9468d11
ParkMob <yeojinp0415@gmail.com>
2021-11-10 20:03:11 -0500

    UPDATE-SYNC

--- install/install.inc2.php
+++
@@ -1,7 +0,0 @@
-<div id="ins_ft">
-    <strong>AVOCADO EDITION</strong>
-    <p>GPL! OPEN SOURCE GNUBOARD</p>
-</div>
-
-</body>
-</html>
--- skin/member/basic/memo_view.skin.php
+++
@@ -1,4 +0,0 @@
-<?php
-if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
-goto_url('./memo_form.php?me_recv_mb_id='.$mb['mb_id']);
-?>
